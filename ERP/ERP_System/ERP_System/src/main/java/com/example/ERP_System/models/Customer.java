540d4d253622f9a1130cde5e50d8d7dfc255a864
unknown <eliasyeshanaw@gmail.com>
2026-02-03 14:04:17 +0300

    fix on customer model

--- ERP/ERP_System/ERP_System/src/main/java/com/example/ERP_System/models/Customer.java
+++ ERP/ERP_System/ERP_System/src/main/java/com/example/ERP_System/models/Customer.java
@@ -16,7 +16,7 @@ public class Customer {
     private String name;
 
     private String email;
-    private String Phone;
-    private String Address;
+    private String phone;
+    private String address;
     private String gstin;
 }
